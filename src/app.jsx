780c7e5decd56c9a9ed6d49a8566259e0324dd85
bbyik <kby00517@naver.com>
2022-04-11 14:50:47 +0900

    Add get API

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react";
 import "./app.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return <h1>Hello~!~!</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyASmKtEI9wy5QhLI3UviyuW1HNKCzHhrZU",
+      requestOptions
+    )
+      // .then((response) => response.text())
+      .then((response) => response.json())
+      // .then((result) => console.log(result))
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  fetch(
+    "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyASmKtEI9wy5QhLI3UviyuW1HNKCzHhrZU",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;

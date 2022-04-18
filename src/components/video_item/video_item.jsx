2912c868fb837bbb107a333fd189481aa90ce834
bbyik <kby00517@naver.com>
2022-04-18 18:16:09 +0900

    Add Css

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import styles from "./video_item.module.css";
 
 // const VideoItem = (props) => (
 const VideoItem = ({ video: { snippet: selItem } }) => (
-  <li>
-    <img src={selItem.thumbnails.medium.url} alt="video thumbnail" />
+  <li className={styles.video}>
+    <img
+      className={styles.thumbnail}
+      src={selItem.thumbnails.medium.url}
+      alt="video thumbnail"
+    />
     <div>
-      <p>{selItem.title}</p>
-      <p>{selItem.channelTitle}</p>
+      <p className={styles.title}>{selItem.title}</p>
+      <p className={styles.channel}>{selItem.channelTitle}</p>
     </div>
   </li>
 );

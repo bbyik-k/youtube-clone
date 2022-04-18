eeb127975911190d4cf4fa376590f18b1881fdfc
bbyik <kby00517@naver.com>
2022-04-18 17:03:44 +0900

    Add videoItem

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-const VideoItem = (props) => <h1>{props.video.snippet.title}</h1>;
+// const VideoItem = (props) => (
+const VideoItem = ({ video: { snippet: selItem } }) => (
+  <li>
+    <img src={selItem.thumbnails.medium.url} alt="video thumbnail" />
+    <div>
+      <p>{selItem.title}</p>
+      <p>{selItem.channelTitle}</p>
+    </div>
+  </li>
+);
 
 export default VideoItem;

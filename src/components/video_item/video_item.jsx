d8fc1d6c487423149088083f0aae5f7893166a97
bbyik <kby00517@naver.com>
2022-04-27 11:27:55 +0900

    Add video items

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -3,15 +3,17 @@ import styles from "./video_item.module.css";
 
 // const VideoItem = (props) => (
 const VideoItem = ({ video: { snippet: selItem } }) => (
-  <li className={styles.video}>
-    <img
-      className={styles.thumbnail}
-      src={selItem.thumbnails.medium.url}
-      alt="video thumbnail"
-    />
-    <div>
-      <p className={styles.title}>{selItem.title}</p>
-      <p className={styles.channel}>{selItem.channelTitle}</p>
+  <li className={styles.container}>
+    <div className={styles.video}>
+      <img
+        className={styles.thumbnail}
+        src={selItem.thumbnails.medium.url}
+        alt="video thumbnail"
+      />
+      <div className={styles.metadata}>
+        <p className={styles.title}>{selItem.title}</p>
+        <p className={styles.channel}>{selItem.channelTitle}</p>
+      </div>
     </div>
   </li>
 );
